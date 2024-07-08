5572cbdd97966608c58e9acc2dc40a9d686547a9
construktdev <construktai@gmail.com>
2024-07-08 17:13:30 +0200

    Add files via upload

---
+++ Discord-Token-Fetcher-main/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Discord_Token_Fetcher
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            string pass = Console.ReadLine();
+            tokenRequest(email, pass);
+        }
+
+        async static Task tokenRequest(string email, string pass)
+        {
+            string url = "https://www.discord.com/api/v9/auth/login";
+
+            var payload = new Dictionary<string, string>(){
+                {"login", email},
+                {"password", pass}
+                
+                };
+            string jsonPayload = JsonConvert.SerializeObject(payload);
+            HttpClient client = new HttpClient();
+            StringContent stringContent = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, stringContent);
+            if(response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("\nConnection Succesfull");
+                string res = await response.Content.ReadAsStringAsync();
+
+                JObject json = JObject.Parse(res);
+                Console.WriteLine($"UserID: {json["user_id"]}");
+                Console.WriteLine($"UserID: {json["token"]}");
+                Console.ReadLine();
+            } else
+            {
+                Console.WriteLine(response.ReasonPhrase);
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
---
+++ Discord-Token-Fetcher-main/README.md
@@ -0,0 +1,10 @@
+# Discord-Token-Fetcher
+Discord Token Fetcher EDUCATIONAL PURPOSES
+
+# I WILL NOT BE RESPONSIBLE FOR ANY DAMAGE THIS PROJECT CAUSES!
+Enjoy!
+
+# How to use:
+-> Get the Email and Password of the Account
+-> Enter it
+-> Enjoy the Token
